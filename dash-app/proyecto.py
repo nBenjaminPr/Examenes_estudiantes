b43f18a4854c7d725cadca12000299cfd1f9a5a5
Nicolas <nicobenpe@gmail.com>
2025-06-11 11:21:16 -0300

    Agregando columna de promedios

--- dash-app/proyecto.py
+++ dash-app/proyecto.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+#Agregando base de datos
 data = pd.read_csv('./data/StudentsPerformance.csv')
 
-print (data.head())
+data['promedio'] = data[['math score', 'reading score', 'writing score']].mean(axis=1).round(2)
+
+print (data)
