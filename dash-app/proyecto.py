a1b83514c45ec929869b78d3d327a0b7fe56557a
NicolasB <nicobenpe@gmail.com>
2025-06-11 23:44:54 -0300

    Pagina en funcionamiento

--- dash-app/proyecto.py
+++ dash-app/proyecto.py
@@ -8,46 +8,48 @@ data = pd.read_csv('C:/Users/ale_v/OneDrive/Desktop/Nico/Proyectos/Examenes_estu
 
 #Añadiendo columna de promedios
 data['promedio'] = data[['math score', 'reading score', 'writing score']].mean(axis=1).round(2)
-print(data)
+
 #diseño con bootstrap 
 
 ###
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 navbar = dbc.NavbarSimple(
-    brand= "Dashboard de estudiantes",
+    brand = 'Dashboard de Estudiantes',
     children=[
         dash.html.A(
             'Fuente de datos',
             href='https://www.kaggle.com/datasets/spscientist/students-performance-in-exams',
-            target= '_blank',
-            style= {'color: white'}
+            target='_blank',
+            style={'color': 'white'}
         )
     ],
     fluid=True
 )
 
 #Creando Menu
-menu= dash.html.Div(
+menu = dash.html.Div(
     dash.dcc.Dropdown(
-        id= "Menu",
-        options= [
-            {"label": "Todos", "value": "todos"},
-            {"label": "Mujer", "value": "mujer"},
-            {"label": "Hombre", "value": "hombre"}
+        id='menu',
+        options=[
+            {'label': 'Todos', 'value':'todos'},
+            {'label': 'Mujer', 'value':'female'},
+            {'label': 'Hombre', 'value':'male'}
         ],
-        value= "todos",
-        style= {"width": "95%", "marginTop": "5px"} 
+        value='todos',
+        clearable=False,
+        searchable=False,
+        style={'width': '95%', 'marginTop':'5px'}
     ),
-    style= {
-        "display": "flex",
-        "justify-content": "center"
+    style={
+        'display':'flex',
+        'justify-content': 'center'
     }
 )
 
 app.layout = dash.html.Div([
     dbc.Row(navbar),
-    dbc.Row(menu)
+    dbc.Row(menu),
 ])
 
 if __name__ == '__main__':
