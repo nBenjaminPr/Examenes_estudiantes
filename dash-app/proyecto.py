6d2d62d7adb85a5d8f26b37e2b3f1a827a64b81d
NicolasB <nicobenpe@gmail.com>
2025-06-11 23:29:22 -0300

    Añadiendo menu

--- dash-app/proyecto.py
+++ dash-app/proyecto.py
@@ -4,12 +4,11 @@ import dash_bootstrap_components as dbc
 import plotly.express as px
 
 #Agregando base de datos
-data = pd.read_csv('C:/Users/Usuario/Desktop/Escriotrio Nico/PROYECTOS/Examenes de alumnos/data/StudentsPerformance.csv')
-
+data = pd.read_csv('C:/Users/ale_v/OneDrive/Desktop/Nico/Proyectos/Examenes_estudiantes/Examenes_estudiantes/data/StudentsPerformance.csv')
 
 #Añadiendo columna de promedios
 data['promedio'] = data[['math score', 'reading score', 'writing score']].mean(axis=1).round(2)
-
+print(data)
 #diseño con bootstrap 
 
 ###
@@ -28,8 +27,27 @@ navbar = dbc.NavbarSimple(
     fluid=True
 )
 
+#Creando Menu
+menu= dash.html.Div(
+    dash.dcc.Dropdown(
+        id= "Menu",
+        options= [
+            {"label": "Todos", "value": "todos"},
+            {"label": "Mujer", "value": "mujer"},
+            {"label": "Hombre", "value": "hombre"}
+        ],
+        value= "todos",
+        style= {"width": "95%", "marginTop": "5px"} 
+    ),
+    style= {
+        "display": "flex",
+        "justify-content": "center"
+    }
+)
+
 app.layout = dash.html.Div([
-    navbar,
+    dbc.Row(navbar),
+    dbc.Row(menu)
 ])
 
 if __name__ == '__main__':
